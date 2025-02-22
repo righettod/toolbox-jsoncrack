70acca17055ab6eb7a0cc4a65b8c78e5098a0a7f
Dominique RIGHETTO <righettod@users.noreply.github.com>
2025-02-22 17:02:26 +0100

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # See this issue for technical details:
 # https://github.com/AykutSarac/jsoncrack.com/issues/434#issuecomment-2630202211
+echo "[+] Clone the jsoncrack repository"
 git clone --depth 1 https://github.com/AykutSarac/jsoncrack.com.git jsoncrack
 cd jsoncrack
+echo "[+] Apply patching operations"
 sed 's/RUN corepack/RUN npm i -g corepack@latest \&\& corepack/g' Dockerfile > DockerfilePatched
 mv DockerfilePatched Dockerfile
+echo "[V] Ready for the build of the image"
