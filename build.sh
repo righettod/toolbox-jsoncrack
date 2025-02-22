3dd6f63d386a638575ed69b0793e657130e1fc24
Dominique RIGHETTO <righettod@users.noreply.github.com>
2025-02-22 11:04:12 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#apt install npm sudo npm install -g pnpm
-export NODE_OPTIONS=--max_old_space_size=1024
-rm -rf jsoncrack.com 2>/dev/null
-git clone --quiet --depth 1 https://github.com/AykutSarac/jsoncrack.com.git jsoncrack.com
-cd jsoncrack.com
-pnpm install
-pnpm dev
+git clone --depth 1 git clone --depth 1 https://github.com/AykutSarac/jsoncrack.com.git jsoncrack
+cd jsoncrack
+sed 's/RUN corepack/RUN npm i -g corepack@latest \&\& corepack/g' Dockerfile > DockerfilePatched
+mv DockerfilePatched Dockerfile
