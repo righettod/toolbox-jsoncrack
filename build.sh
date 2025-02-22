c6210540c827eb627bdf0402c2ff635c7d6d736f
Dominique RIGHETTO <righettod@users.noreply.github.com>
2025-02-22 11:12:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# See this issue for technical details:
+# https://github.com/AykutSarac/jsoncrack.com/issues/434#issuecomment-2630202211
 git clone --depth 1 https://github.com/AykutSarac/jsoncrack.com.git jsoncrack
 cd jsoncrack
 sed 's/RUN corepack/RUN npm i -g corepack@latest \&\& corepack/g' Dockerfile > DockerfilePatched
