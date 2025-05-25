f796c1fb2a78e229071d4b62df9b63eb85bef5d3
Dominique RIGHETTO <righettod@users.noreply.github.com>
2025-05-25 10:32:41 +0200

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -5,8 +5,8 @@ echo "[+] Clone the jsoncrack repository"
 git clone --depth 1 https://github.com/AykutSarac/jsoncrack.com.git jsoncrack
 cd jsoncrack
 echo "[+] Apply patching operations"
-sed 's/RUN corepack/RUN npm i -g corepack@latest \&\& corepack/g' Dockerfile > DockerfilePatched
-sed 's/--frozen-lockfile/--no-frozen-lockfile/g' DockerfilePatched > DockerfilePatched2
-mv DockerfilePatched2 DockerfilePatched
-mv DockerfilePatched Dockerfile
+sed 's/RUN corepack/RUN npm i -g corepack@latest \&\& corepack/g' Dockerfile > DockerfilePatched1
+sed 's/--frozen-lockfile/--no-frozen-lockfile/g' DockerfilePatched1 > DockerfilePatched2
+mv DockerfilePatched2 DockerfilePatched1
+mv DockerfilePatched1 Dockerfile
 echo "[V] Ready for the build of the image"
